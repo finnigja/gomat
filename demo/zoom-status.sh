9a22ff9edcdcdd0714324b25a59737c08d265c4b
Jamie Finnigan <jfinnigan@hashicorp.com>
2024-10-21 08:35:30 -0700

    make looper less noisy

--- demo/zoom-status.sh
+++ demo/zoom-status.sh
@@ -6,22 +6,23 @@ while sleep 1; do
     ZOOM="off"
     PROC_COUNT=`ps aux | grep 'CptHost' | grep -v 'grep' | wc -l`
     if [[ $PROC_COUNT -gt "0" ]]; then
-        echo 'MEETING IS ACTIVE'
         ZOOM="on"
-    else
-        echo 'no meeting currently active'
     fi
     STATE=`cat ~/.gomat/device_status`
     if $FIRST; then
         if [[ $ZOOM == "on" ]]; then
+            echo `date` ' Zoom meeting active, turning light on'
             ./nightlight-on.sh
         elif [[ $ZOOM == "off" ]]; then
+            echo `date` ' no active Zoom meeting, turning light off'
             ./nightlight-off.sh
         fi
     else
         if [[ $ZOOM == "on" && $STATE == "off" ]]; then
+            echo `date` ' Zoom meeting active, turning light on'
             ./nightlight-on.sh
         elif [[ $ZOOM == "off" && $STATE == "on" ]]; then
+            echo `date` ' no active Zoom meeting, turning light off'
             ./nightlight-off.sh
         fi
     fi
